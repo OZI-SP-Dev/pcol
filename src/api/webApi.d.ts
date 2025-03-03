787de1a35a092dec0a6f3885a3b3e7aeeb40a6d9
robertporterfield <57455266+RobertPorterfield@users.noreply.github.com>
2025-03-03 11:17:47 -0500

    change to call that works for all users

--- src/api/webApi.d.ts
+++ src/api/webApi.d.ts
@@ -1,2 +1,2 @@
 import "@pnp/sp/webs";
-export declare const useWebs: () => import("@tanstack/react-query").UseQueryResult<import("@pnp/sp/webs").IWebInfo[], Error>;
+export declare const useWebs: () => import("@tanstack/react-query").UseQueryResult<import("@pnp/sp/webs").IWebInfosData[], Error>;
--- src/api/webApi.ts
+++ src/api/webApi.ts
@@ -5,7 +5,7 @@ import "@pnp/sp/webs";
 export const useWebs = () => {
   return useQuery({
     queryKey: ["Webs"],
-    queryFn: () => spWebContext.web.webs(),
+    queryFn: () => spWebContext.web.getSubwebsFilteredForCurrentUser(-1, -1)(),
     staleTime: Infinity, // Prevent refetch
     gcTime: Infinity, // Prevent garbage collection
   });
