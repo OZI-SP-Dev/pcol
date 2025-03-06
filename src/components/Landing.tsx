b796cae4b3f5c8aefd8951b18b351a44b3570b84
robertporterfield <57455266+RobertPorterfield@users.noreply.github.com>
2025-03-06 11:25:09 -0500

    Landing page freshen up

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -6,20 +6,38 @@ const Landing = () => {
   const webs = useWebs();
 
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        alignItems: "center",
+      }}
+    >
+      <br />
       <Title2>Welcome to the PCOL Tool</Title2>
       <br />
+      <br />
       {webs.isLoading && <div>Fetching data...</div>}
+      {webs.isError && (
+        <div>
+          There was an error! Reload to try again, or view the{" "}
+          <Link to="/help">Help</Link> page.
+        </div>
+      )}
       {webs.data && webs.data.length > 0 && (
         <>
           <section>
-            <Title3>Please select the org for your PCOLs</Title3>
-            {webs.data.map((web) => (
-              <div key={web.Title}>
-                <Link to={"/p/" + web.Title}>{web.Title}</Link>
-              </div>
-            ))}
+            <Title3>Please click the org link for your PCOLs</Title3>
+            <ul>
+              {webs.data.map((web) => (
+                <li key={web.Title}>
+                  <Link to={"/p/" + web.Title}>{web.Title}</Link>
+                </li>
+              ))}
+            </ul>
           </section>
+          <br />
           <section>
             Program not listed? See the <Link to="/help">Help</Link> page.
           </section>
@@ -31,7 +49,7 @@ const Landing = () => {
           <Link to="/help">Help</Link> page.
         </>
       )}
-    </>
+    </div>
   );
 };
 
