d930a7c2925283e9c23b44f5af7758b709eab0b4
robertporterfield <57455266+RobertPorterfield@users.noreply.github.com>
2025-03-07 09:29:32 -0500

    simplify permissionsApi

--- src/api/permissionsApi.ts
+++ src/api/permissionsApi.ts
@@ -4,25 +4,18 @@ import "@pnp/sp/security";
 import { PermissionKind } from "@pnp/sp/security";
 import { spWebContext, subWebContext } from "./SPWebContext";
 
-declare const _spPageContextInfo: { webServerRelativeUrl: string };
-
 /* Since the Disclaimers list is universal, we can check it on both the base site, 
 // and each program's site to see if the user has edit rights.
 // This way we don't worry about groups names across the sites, just 'can this user, or not?'
 */
 export const useCurrentUserHasEditPermissions = (subSite?: string) => {
-  const listUrl =
-    _spPageContextInfo.webServerRelativeUrl +
-    (subSite ? "/" + subSite : "") +
-    "/Lists/Disclaimers";
-
   const context = subSite ? subWebContext(subSite) : spWebContext;
 
   return useQuery({
-    queryKey: ["EditPermissions", listUrl],
+    queryKey: ["EditPermissions", subSite ?? "Root"],
     queryFn: () =>
       context.web
-        .getList(listUrl)
+        .lists.getByTitle("Disclaimers")
         .currentUserHasPermissions(PermissionKind.EditListItems),
     staleTime: Infinity, // Prevent refetch
     gcTime: Infinity, // Prevent garbage collection
