93073d73d9d7a402343e45e5efeaace1e5979a4c
robertporterfield <57455266+RobertPorterfield@users.noreply.github.com>
2025-07-07 09:09:09 -0400

    Display roles

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,7 +1,16 @@
-import { Text, makeStyles } from "@fluentui/react-components";
+import {
+  Avatar,
+  Popover,
+  PopoverSurface,
+  PopoverTrigger,
+  Text,
+  Tooltip,
+  makeStyles,
+} from "@fluentui/react-components";
 import { NavLink, useMatch } from "react-router";
 import { tokens } from "@fluentui/react-theme";
 import { useCurrentUserHasEditPermissions } from "src/api/permissionsApi";
+import { useMyRoles } from "src/api/Roles/rolesApi";
 
 /* FluentUI Styling */
 const useStyles = makeStyles({
@@ -48,6 +57,12 @@ const useStyles = makeStyles({
   navAvatar: { marginLeft: "1em", marginRight: "5px" }, // Force the Avatar icon to be positioned at the right most side
 });
 
+declare const _spPageContextInfo: {
+  userId: number;
+  userDisplayName: string;
+  userEmail: string;
+};
+
 export const AppHeader = () => {
   const classes = useStyles();
   const match = useMatch({ path: "/p/:program", end: false });
@@ -55,6 +70,8 @@ export const AppHeader = () => {
 
   const mainPerms = useCurrentUserHasEditPermissions();
   const programPerms = useCurrentUserHasEditPermissions(program);
+
+  const roles = useMyRoles(String(program));
 
   const showAdmin = mainPerms.data || programPerms.data;
 
@@ -88,6 +105,35 @@ export const AppHeader = () => {
         >
           Help
         </NavLink>
+        <Popover trapFocus={true} closeOnScroll={true} withArrow={true}>
+          <PopoverTrigger>
+            <Tooltip
+              relationship="description"
+              content={_spPageContextInfo.userDisplayName}
+            >
+              <Avatar
+                className={classes.navAvatar}
+                image={{
+                  src:
+                    "/_layouts/15/userphoto.aspx?accountname=" +
+                    _spPageContextInfo.userEmail +
+                    "&size=S",
+                }}
+                name={_spPageContextInfo.userDisplayName}
+                size={32}
+              ></Avatar>
+            </Tooltip>
+          </PopoverTrigger>
+          <PopoverSurface aria-label="Your roles">
+            <ul>
+              {roles.isPKAdmin && <li>PK Admin</li>}
+              {roles.isAdmin && <li>Program Admin</li>}
+              {roles.isCO && <li>CO</li>}
+              {roles.isDistributor && <li>Distributor</li>}
+              {roles.isReviewer && <li>Reviewer</li>}
+            </ul>
+          </PopoverSurface>
+        </Popover>
       </div>
       {/* The below div is set to the same height of the above div,
               to ensure all content loaded has proper padding at the top so that it isn't below the header */}
