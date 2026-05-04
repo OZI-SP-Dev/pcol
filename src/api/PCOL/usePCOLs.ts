a442960cadd399bb909db881bac785e1a138441d
robertporterfield <57455266+RobertPorterfield@users.noreply.github.com>
2026-05-04 16:05:24 -0400

    remove unneeded select/expand

--- src/api/PCOL/usePCOLs.ts
+++ src/api/PCOL/usePCOLs.ts
@@ -146,7 +146,7 @@ const firstPageQuery = (
   filterParams: PCOLFilter[],
   allItems: boolean,
 ) => {
-  const requestedFields = "Id,Modified,Title,Subject,Contract,Stage,Author/Id";
+  const requestedFields = "Id,Modified,Title,Subject,Contract,Stage";
 
   let queryString = "ContentType eq 'PCOLDocSet'";
   if (!allItems) {
@@ -161,7 +161,6 @@ const firstPageQuery = (
     .web.lists.getByTitle("PCOLs")
     .items.select(requestedFields)
     .filter(queryString)
-    .expand("Author")
     .orderBy(
       sortParams.sortColumn?.toString() || "Created",
       sortParams.sortDirection !== "descending",
